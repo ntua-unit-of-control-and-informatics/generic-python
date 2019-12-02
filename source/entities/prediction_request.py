ef4c2ef3ac74354331e5c71bffd2a820218433de
Pantelispanka <pantelispanka@gmail.com>
2019-12-02 16:27:50 +0200

    main

--- source/entities/prediction_request.py
+++
@@ -1,6 +0,0 @@
-class PredictionRequest:
-
-    def __init__(self, dataset, rawModel, additionalInfo):
-        self.dataset = dataset
-        self.rawModel = rawModel
-        self.additionalInfo = additionalInfo
